dc01cc58d6a327f218bda17cbd008df1a868829b
Lulzim Zenelaj <lz47429@ubt-uni.net>
2022-02-22 16:51:25 +0100

    first commit

---
+++ express/api/Controllers/admController.js
@@ -0,0 +1,40 @@
+import admin from '../services/firebase';
+const db = admin.firestore();
+
+
+
+
+
+
+
+const admController = {
+    addAdmin: async (req, res) => {
+        const { email } = req.body;
+        try {
+            const user = await admin.auth().getUserByEmail(email)
+
+            if (!user.customClaims) {
+                admin.auth().setCustomUserClaims(user.uid, {
+                    admin: true,
+                });
+                db.collection('roles').doc(user.uid).set({
+                    email: user.email,
+                    role: { admin: true }
+                })           
+                 return res.json({        
+                     message: `${email} has been made as admin!`,
+                });
+            }
+            else{
+                return res.json({message:"This user is alredy an admin"})
+            }
+        } catch (err) {
+            console.log(err.message);
+            return res.json({
+                message: err.message
+            })
+        }
+    }
+}
+
+export default admController;
